232af82a2b6fce0a30dd040706a3611ce32ff69d
selmaBBK <selma.boubakri@hotmail.com>
2021-05-05 14:59:53 +0200

    add form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 class ContactController extends AbstractController
 {
-public function index()
-{
-    return $this->twig->render('Contact/contact.html.twig');
-}
+    public function index()
+    {
+        return $this->twig->render('Contact/contact.html.twig');
+    }
 }
